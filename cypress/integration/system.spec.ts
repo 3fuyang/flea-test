134d65f8af018d471d81bf1b03eb54bceffeb30b
fwio <624835339@qq.com>
2022-06-24 12:13:30 +0800

    Update config

--- cypress/integration/system.spec.ts
+++ cypress/integration/system.spec.ts
@@ -1,10 +1,4 @@
-// system.spec.ts created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
-
+/// <reference types="cypress">
 describe('E2E test for flea-test application', () => {
   before(() => {
     cy.request('http://106.15.78.201:8086/api/resetDB')
